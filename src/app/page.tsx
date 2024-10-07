302ec2abd5b090a882fc43acf9879b4bf6cd9471
Alessandro Sottile <sottile-124@gmail.com>
2024-10-07 11:06:22 +0200

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,26 @@ import { ContactSection } from "@/sections/Contact";
 import { Footer } from "@/sections/Footer";
 import { ExpSection } from "@/sections/Experience";
 
-
+<span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">Data Scientist</span>
 export default function Home() {
   return (
     <div>
       <Header />
       <HeroSection />
       <div className="container">
-      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6"> Highlighted Projects </h2>
+      <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400  text-center bg-clip-text text-transparent">
+      My Vision
+      </p>
+      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6"> Driven by Innovation  </h2>
       <div className="md:text-lg lg:text-xl text-white/60  text-center mt-6 pb-16 lg:py-24"> {/* Add margin here */}
         <p className=" md:text-lg lg:text-xl text-white/60 mt-2">
-        I am a <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">Data Scientist</span> with a solid academic background in Data Science and Statistics from Sapienza University of Rome. I specialize in machine learning and data management, having worked at several companies to develop <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">AI solutions</span> that enhance decision-making. I am interested in helping businesses leverage artificial intelligence for growth, both in the <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">Tuscia Viterbese</span> region and beyond. I believe innovation is key to future success.</p>
+        I am a <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">Data Scientist</span> passionate about innovation and technology, with a strong academic background in <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">Data Science</span> and <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">Statistics</span> from "Sapienza" University of Rome. I possess advanced skills in <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">machine learning</span>, <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">deep learning</span>, and <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">data management</span>, and have worked on projects ranging from text analysis to image classification, predictive modeling, and data-driven decision systems. My expertise spans <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">various industries</span>, allowing me to adapt solutions to different challenges.</p>
+        <br></br>
+        <p className=" md:text-lg lg:text-xl text-white/60 mt-2">
+        I have participated in various projects, both within companies and at the university, focusing on <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">AI development</span>, <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">data management</span>, and <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">analysis</span> to create tailored solutions that enhance decision-making and optimize data usage. Through these projects and collaborations, I have helped in <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">leverage AI</span> for smarter decision-making. I believe in the transformative power of technology to improve business efficiency and competitiveness.</p>
+        <br></br>
+        <p className=" md:text-lg lg:text-xl text-white/60 mt-2">
+        I am seeking <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">exciting opportunities</span> to apply my skills and contribute to <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">innovative solutions</span>. My goal is to assist companies, including those in the <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">Tuscia Viterbese</span> region, in leveraging artificial intelligence to tackle the challenges of a rapidly evolving market. For more on how I’ve applied my expertise in AI and data science to solve real-world challenges, explore my work experiences and projects below. I am convinced that innovation is essential for <span className="font-semibold bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">future success</span>, both locally and globally.</p>    
       </div>
       </div>
       <ExpSection />
--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -51,35 +51,34 @@ export const ExpSection = () => {
 
 
   <div className="mt-20 flex flex-col gap-8">
-    <div className=' grid grid-cols-1 gap-8 md:grid-cols-4 lg:grid-cols-1'>
+    <div className=' grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2'>
       
     <Card className="h-[320px] md:col-span-1 lg:col-span-1">
-<CardHeader title="Master Degree in Data Science" 
-description="During my university course, delivered entirely in English, I collaborated on original projects using state-ofthe-art technologies in the fields of deep learning, machine learning, data analysis, and data management.
-These projects dealt with a wide range of data, including texts, numbers, images, and 3D scans. The
-projects are available for consultation on my Github repository.
+<CardHeader title="MSc in Data Science" 
+description="During my university course, delivered entirely in English, I collaborated on original projects using state-of-the-art technologies in the fields of deep learning, machine learning, data analysis, and data management.
+These projects dealt with a wide range of data, including texts, numbers, images, and 3D scans.
 " />
-<div className="w-40 mx-auto mt-2 md:mt-0">
-<Image src={print_3d} alt="Book cover" />
-</div>
+
 
 
     </Card>
     <Card className="h-[320px]  md:col-span-1 lg:col-span-1">
     <CardHeader
     title="Trainingcamp Leonardo SpA"
-    description="Uncover the Cutting-Edge Tools and Technologies Behind Data-Driven and AI-Powered Solutions"
+    description=" During the Training Camp, I enhanced my skills in the field of
+deep learning through theoretical and practical lessons. Applying this knowledge to the task of image
+classification on photos of various categories of ships."
     className=""
   />
 </Card>
 
 <Card className="h-[320px] md:col-span-1 lg:col-span-1">
-<CardHeader title="Bachelor Degree in statistics" 
-description="Discover the endless possibilities of 3D printing! 
- I am open to commissions." />
-<div className="w-40 mx-auto mt-2 md:mt-0">
-<Image src={print_3d} alt="Book cover" />
-</div>
+<CardHeader title="Bachelor in statistics" 
+description="During my studies, I acquired a solid foundation of knowledge in statistical disciplines,
+both theoretical and applied, complemented by extensive training in Computer Science, Mathematics, and
+Probability.
+" />
+
 
 
     </Card>
@@ -96,24 +95,6 @@ description="Discover the endless possibilities of 3D printing!
 </div>
 <div className='grid grid-cols-1 md:grid-cols-4 lg:grid-cols-2 gap-8'>
 
-
-<Card className='h-[320px] p-0 flex flex-col md:col-span-2 lg:col-span-1'>
-<CardHeader title="Beyond the Code" 
-description="Explore my interests and hobbies beyond the digital realm" className='px-6 py-6' />
-  <div className='relative flex-1' ref= {constraintRef }>
-
-
-  </div>
-</Card>
-<Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
- <Image src={mapImage}  alt="map" className='h-full w-full object-cover absolute object-left-top'/>
- <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full 
- after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-2 after:rounded-full after:outline-gray-950/30">
-  <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
-  <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
- <Image src={smileMemoji}  alt="smiling memoji" className="size-20" />
- </div>
-</Card>
 </div>
   </div>
   </div>
